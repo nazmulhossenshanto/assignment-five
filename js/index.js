72db9707c57ecee50a61dfa237be00d27e29108e
shanto <mdnazmulhossenshanto01@gmail.com>
2025-04-02 04:00:17 +0600

    added javascript for donate button

--- js/index.js
+++ js/index.js
@@ -1,2 +1,48 @@
 console.log('index.js')
 
+// Donate for Noakhali
+document.getElementById('donate-noakhali-btn').addEventListener('click', function () {
+    const mainBalance = convertStringToNumber('main-balance');
+    const donateAmountNoakhali = getInputValue('donate-amount-noakhali');
+    const newBalance = document.getElementById('main-balance');
+    const noakhaliBalace = convertStringToNumber('noakhali-balance');
+    const newBalanceNoakhali = document.getElementById('noakhali-balance');
+    if (isNaN(donateAmountNoakhali) || donateAmountNoakhali <= 0) {
+        alert('Please enter a valid number')
+        return;
+    }
+    newBalance.innerText = mainBalance - donateAmountNoakhali;
+    newBalanceNoakhali.innerText = noakhaliBalace + donateAmountNoakhali;
+    document.getElementById('donate-amount-noakhali').value = '';
+});
+// Donate for Feni
+document.getElementById('donate-feni-btn').addEventListener('click', function () {
+    const mainBalance = convertStringToNumber('main-balance');
+    const donateAmountFeni = getInputValue('donate-amount-feni');
+    const newBalance = document.getElementById('main-balance');
+    const feniBalace = convertStringToNumber('feni-balance');
+    const newBalancefeni = document.getElementById('feni-balance');
+    if (isNaN(donateAmountFeni) || donateAmountFeni <= 0) {
+        alert('Please enter a valid number')
+        return;
+    }
+    newBalance.innerText = mainBalance - donateAmountFeni;
+    newBalancefeni.innerText = feniBalace + donateAmountFeni;
+    document.getElementById('donate-amount-feni').value = '';
+});
+// Donate for injured in quota movement
+document.getElementById('donate-quota-btn').addEventListener('click', function () {
+    const mainBalance = convertStringToNumber('main-balance');
+    const donateAmountQuota = getInputValue('donate-quota-amount');
+    const newBalance = document.getElementById('main-balance');
+    const QuotaBalace = convertStringToNumber('quota-balance');
+    const newBalanceQuota = document.getElementById('quota-balance');
+    if (isNaN(donateAmountQuota) || donateAmountQuota <= 0) {
+        alert('Please enter a valid number')
+        return;
+    }
+    newBalance.innerText = mainBalance - donateAmountQuota;
+    newBalanceQuota.innerText = QuotaBalace + donateAmountQuota;
+    document.getElementById('donate-quota-amount').value = '';
+});
+
--- js/library.js
+++ js/library.js
@@ -1,1 +1,11 @@
 console.log('Library file added to html file.')
+function convertStringToNumber(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = Number(textValue);
+    return textNumber;
+}
+function getInputValue(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = Number(inputValue);
+    return inputNumber;
+}
